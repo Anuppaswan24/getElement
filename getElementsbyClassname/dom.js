e01e618fce58340d333ded40b93911c2ae90b23f
Anuppaswan24 <119486812+Anuppaswan24@users.noreply.github.com>
2023-03-23 00:02:11 -0700

    Add files via upload

---
+++ getElementsbyClassname/dom.js
@@ -0,0 +1,11 @@
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[3]);
+items[0].style.fontWeight='bold';
+items[1].style.fontWeight='bold';
+items[2].style.fontWeight='bold';
+items[3].style.fontWeight='bold';
+console.log(items);
+console.log(items[2]);
+items[2].style.backgroundColor = 'green';
+
